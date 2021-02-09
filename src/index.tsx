1f0b7bf637760c3bfb2671f80db6f81770673d0d
Trisha Gonzales <gonzalestrishaanne@gmail.com>
2021-02-10 01:02:45 +0800

    chore: setup boilerplate

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
+import { ToastProvider } from 'react-toast-notifications';
+import { ApolloProvider } from '@apollo/client';
+import { client } from './api/client';
+import '@fortawesome/fontawesome-free/css/all.css';
+
+import { App } from './pages/App';
+import { Loading } from './components/common/Loading';
+import { Providers } from './providers/Providers';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback={<Loading />}>
+      <BrowserRouter>
+        <ToastProvider placement='top-center' autoDismiss={true}>
+          <ApolloProvider client={client}>
+            <Providers>
+              <App />
+            </Providers>
+          </ApolloProvider>
+        </ToastProvider>
+      </BrowserRouter>
+    </Suspense>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
