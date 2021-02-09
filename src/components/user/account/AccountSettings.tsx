9aafdfa2fc8104629678929ce01c86aa757fc3c6
Trisha Gonzales <gonzalestrishaanne@gmail.com>
2021-02-10 02:11:15 +0800

    feat: account settings page

---
+++ src/components/user/account/AccountSettings.tsx
@@ -0,0 +1,50 @@
+/* eslint-disable import/first */
+import React from 'react';
+import styled from 'styled-components';
+import { useEdit } from '../../../hooks/useEdit';
+import { useModal } from '../../../hooks/portal/useModal';
+import { lazy } from '../../../utils/dynamicImports.util';
+
+import { Row } from '../../common/Layout';
+import { Button } from '../../common/Button';
+
+const Formik = lazy(() => import('formik'), 'Formik');
+const Form = lazy(() => import('formik'), 'Form');
+const FormikField = lazy(() => import('../../common/FormikField'), 'FormikField');
+
+export interface AccountSettingsProps {}
+
+const AccountSettings: React.FC<AccountSettingsProps> = () => {
+  const { EditButton } = useEdit();
+  const { Modal, open } = useModal();
+
+  return (
+    <Div>
+      <Formik initialValues={{ email: '', password: '' }} onSubmit={() => {}}>
+        {() => (
+          <Form>
+            <Row className='heading' justifyContent='space-between'>
+              <h1>SETTINGS</h1>
+              <EditButton />
+            </Row>
+
+            <FormikField name='username' label='username' />
+            <FormikField name='email' label='email' />
+            <FormikField name='password' label='password' />
+
+            <Button onClick={open}>Open modal</Button>
+            <Modal>Yey</Modal>
+
+            <Row className='actions'>
+              <Button primary>SAVE CHANGES</Button>
+            </Row>
+          </Form>
+        )}
+      </Formik>
+    </Div>
+  );
+};
+
+export default AccountSettings;
+
+const Div = styled.div``;
---
+++ src/pages/AccountPage.tsx
@@ -0,0 +1,112 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useTabs } from '../hooks/useTabs';
+import { device } from '../theme';
+import { lazy } from '../utils/dynamicImports.util';
+
+import { PageHeader, PageTitle } from '../components/global/PageHeader';
+import { Container } from '../components/common/Layout';
+
+const Profile = lazy(() => import('../components/user/account/Profile'));
+const AccountSettings = lazy(() => import('../components/user/account/AccountSettings'));
+
+export interface AccountPageProps {}
+
+const AccountPage: React.FC<AccountPageProps> = () => {
+  const { TabContainer, TabHeader, Tab, TabBody, TabContent } = useTabs({
+    initialTab: 'profile',
+    isVertical: true,
+  });
+
+  return (
+    <>
+      <PageHeader>
+        <PageTitle>ACCOUNT</PageTitle>
+      </PageHeader>
+
+      <Div>
+        <Container>
+          <TabContainer className='tab-container'>
+            <TabHeader className='tab-header'>
+              <Tab className='tab' name='profile'>
+                <span className='text'>Profile</span>
+                <i className='fas fa-user-circle'></i>
+              </Tab>
+              <Tab className='tab' name='settings'>
+                <span className='text'>Settings</span>
+                <i className='fas fa-cog'></i>
+              </Tab>
+            </TabHeader>
+
+            <TabBody className='tab-body'>
+              <TabContent name='profile'>
+                <Profile />
+              </TabContent>
+              <TabContent name='settings'>
+                <AccountSettings />
+              </TabContent>
+            </TabBody>
+          </TabContainer>
+        </Container>
+      </Div>
+    </>
+  );
+};
+
+export default AccountPage;
+
+const Div = styled.div`
+  .tab-header {
+    padding-top: 30px;
+    background: var(--bg);
+    .tab {
+      color: var(--fg2);
+      font-family: Roboto;
+      i {
+        display: none;
+      }
+    }
+    .active {
+      background: white;
+      border-left: 2px solid var(--main);
+      color: var(--main);
+      font-weight: bold;
+    }
+  }
+
+  .tab-body {
+    max-width: 500px;
+    padding: 1.5em;
+    padding-left: 7%;
+    .heading {
+      margin-bottom: 20px;
+    }
+    .actions {
+      margin-top: 30px;
+    }
+  }
+
+  @media (max-width: 760px) {
+    .tab-header {
+      flex: 0 0 45px;
+      .tab {
+        width: 50px;
+        .text {
+          display: none;
+        }
+        i {
+          display: inline;
+          font-size: 20px;
+        }
+      }
+    }
+  }
+
+  @media ${device.phone} {
+    .actions {
+      button {
+        width: 100%;
+      }
+    }
+  }
+`;
