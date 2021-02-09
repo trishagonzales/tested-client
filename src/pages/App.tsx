fafead6d4f75239dd8fc99784f219dfca43a5338
Trisha Gonzales <gonzalestrishaanne@gmail.com>
2021-02-10 01:30:09 +0800

    feat: add app component and routes

---
+++ src/pages/App.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react';
+import { GlobalStyle } from './GlobalStyle';
+import { useUser } from '../hooks/user/useUser';
+import { useCart } from '../hooks/user/useCart';
+import { useWishlist } from '../hooks/user/useWishlist';
+import { Routes, globalRoutes, userRoutes, adminRoutes, checkoutRoutes } from './Routes';
+
+import { Navbar, NavbarMobile } from '../components/global/Navbar/Navbar';
+import { Footer } from '../components/global/Footer';
+import { useOrder } from '../hooks/useOrder';
+
+export function App() {
+  const { user, isAdmin, isAdminAPI, getUserData } = useUser();
+  const { getCartItems, items: cartItems } = useCart();
+  const { getWishlistItems } = useWishlist();
+  const { getOrderItems } = useOrder();
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+
+  useEffect(() => {
+    if (user) {
+      getCartItems();
+      getWishlistItems();
+      getOrderItems();
+      isAdminAPI();
+    }
+  }, [user]);
+
+  return (
+    <>
+      <GlobalStyle />
+      <Navbar />
+      <NavbarMobile />
+
+      <div id='content'>
+        <Routes visible={isAdmin} routes={adminRoutes} />
+        <Routes visible={cartItems && cartItems.length !== 0} routes={checkoutRoutes} />
+        <Routes visible={!!user} routes={userRoutes} />
+        <Routes visible={true} routes={globalRoutes} />
+      </div>
+
+      <Footer />
+    </>
+  );
+}
---
+++ src/pages/Routes.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { RouteProps, Route, Switch } from 'react-router-dom';
+import { lazy } from '../utils/dynamicImports.util';
+import OrdersPage from './OrdersPage';
+
+const HomePage = lazy(() => import('./HomePage'));
+const LoginSignupPage = lazy(() => import('./LoginSignupPage'));
+const ProductPage = lazy(() => import('./ProductPage'));
+const AccountPage = lazy(() => import('./AccountPage'));
+const BuyPage = lazy(() => import('./BuyPage'));
+const WishlistPage = lazy(() => import('./WishlistPage'));
+const LeaveReviewPage = lazy(() => import('./LeaveReviewPage'));
+const AdminPage = lazy(() => import('./AdminPage'));
+const AddProduct = lazy(() => import('../components/admin/AddProduct'));
+const EditProduct = lazy(() => import('../components/admin/EditProduct'));
+
+export interface RoutesProps {
+  routes: RouteProps[];
+  visible: boolean;
+}
+
+export const Routes: React.FC<RoutesProps> = ({ visible, routes }) => {
+  return <Switch>{visible ? routes.map((route, i) => <Route key={i} {...route} />) : null}</Switch>;
+};
+
+export const globalRoutes = [
+  { path: '/product/:id', component: ProductPage },
+  { path: '/login', component: LoginSignupPage },
+  { path: '/signup', component: LoginSignupPage },
+  { exact: true, path: '/', component: HomePage },
+];
+
+export const userRoutes = [
+  { path: '/leave-review/:id', component: LeaveReviewPage },
+  { path: '/orders', component: OrdersPage },
+  { path: '/order-completed', component: BuyPage },
+  { path: '/cart', component: BuyPage },
+  { path: '/wishlists', component: WishlistPage },
+  { path: '/account', component: AccountPage },
+];
+
+export const adminRoutes = [
+  { path: '/admin/add-product', component: AddProduct },
+  { path: '/admin/edit-product/:id', component: EditProduct },
+  { path: '/admin/settings', component: AddProduct },
+  { exact: true, path: '/admin', component: AdminPage },
+];
+
+export const checkoutRoutes = [{ path: '/checkout', component: BuyPage }];
+// export const orderCompleteRoutes = [{ path: '/order-completed', component: BuyPage }];
