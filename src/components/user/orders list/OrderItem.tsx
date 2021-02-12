74bf072ee9b431cac74b76853b16d54dee17ab44
Trisha Gonzales <gonzalestrishaanne@gmail.com>
2021-02-13 00:59:08 +0800

    refactor: extract OrderItem.tsx and OrderStatus.tsx to new components from OrdersPage

---
+++ src/components/user/orders list/OrderItem.tsx
@@ -0,0 +1,124 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { OrderType } from '../../../types/User.types';
+import { BaseProps } from '../../../types/General.types';
+
+import { Row } from '../../common/Layout';
+import { LinkButton } from '../../common/Button';
+import Price from '../../product/common/Price';
+import ProductThumbnail from '../../product/ProductThumbnail';
+import { OrderStatus } from './OrderStatus';
+
+export interface OrderItemProps extends BaseProps {
+  order: OrderType;
+  removeOrder: any;
+}
+
+const OrderItem: React.FC<OrderItemProps> = ({ className, order, removeOrder }) => {
+  return (
+    <Div className={className}>
+      <div
+        className='remove-btn hoverable-underline error-color'
+        onClick={() => removeOrder(order.id)}>
+        DELETE
+      </div>
+
+      {order.items.map((item, index) =>
+        item.product ? (
+          <Link to={'/product/' + item.product.id}>
+            <Row className='product' key={index}>
+              <ProductThumbnail className='thumbnail' src={item.product?.thumbnail} size='70px' />
+
+              <div className='product-info'>
+                <div className='name'>{item.product?.name}</div>
+                <div className='quantity-price'>
+                  <span className='quantity'>x {item.quantity}</span>
+                  <Price fontSize='14px'>{item.product?.price * item.quantity}</Price>
+                </div>
+              </div>
+            </Row>
+          </Link>
+        ) : (
+          <span className='product-removed' key={index}>
+            <i className='fas fa-ban'></i> PRODUCT REMOVED
+          </span>
+        )
+      )}
+
+      <hr />
+
+      <OrderStatus status={order.status} />
+
+      <hr />
+
+      <Row className='total-price' justifyContent='space-between'>
+        {order.status === 'COMPLETED' && !order.isReviewed && (
+          <LinkButton textOnly to={'/leave-review/' + order.id}>
+            Leave review
+          </LinkButton>
+        )}
+        <span></span>
+        <Price fontSize='16px' label='Total'>
+          {order.totalPrice}
+        </Price>
+      </Row>
+    </Div>
+  );
+};
+
+export default OrderItem;
+
+const Div = styled.div`
+  width: 100%;
+  margin: 0.5em 0;
+  padding: 1.5em 1em;
+  position: relative;
+  border: 2px solid var(--lightgrey);
+  border-radius: 5px;
+
+  .product {
+    padding: 0.5em;
+    :hover {
+      background: var(--bg);
+    }
+
+    .thumbnail {
+      margin-right: 1.2em;
+    }
+    .name {
+      margin-bottom: 0.6em;
+    }
+    .quantity-price {
+      font-size: 14px;
+      .quantity {
+        margin-right: 1em;
+      }
+    }
+  }
+
+  .product-removed {
+    margin-left: 1em;
+    color: var(--fg2);
+    font-size: 14px;
+    font-weight: bold;
+  }
+
+  hr {
+    margin: 1em 0.7em;
+  }
+
+  .total-price {
+    margin-top: 1em;
+    button {
+      font-size: 12px;
+    }
+  }
+
+  .remove-btn {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    font-size: 11px;
+  }
+`;
--- src/pages/OrdersPage.tsx
+++ src/pages/OrdersPage.tsx
@@ -4,11 +4,9 @@ import { useOrder } from '../hooks/product/useOrder';
 import { device } from '../theme';
 
 import { PageHeader, PageTitle, PageHeaderButtons } from '../components/global/PageHeader';
-import { Button, LinkButton } from '../components/common/Button';
-import { Row, Container } from '../components/common/Layout';
-import { OrderStatus } from '../components/user/orders list/OrderStatus';
-import ProductThumbnail from '../components/product/ProductThumbnail';
-import Price from '../components/product/common/Price';
+import { Button } from '../components/common/Button';
+import { Container } from '../components/common/Layout';
+import OrderItem from '../components/user/orders list/OrderItem';
 
 export interface OrdersPageProps {}
 
@@ -27,49 +25,7 @@ const OrdersPage: React.FC<OrdersPageProps> = () => {
       <Container>
         <Div>
           {orders.map((order, index) => (
-            <div className='order-item' key={index}>
-              <div
-                className='remove-btn hoverable-underline error-color'
-                onClick={() => removeOrder(order.id)}>
-                DELETE
-              </div>
-
-              {order.items.map((item, index) => (
-                <Row className='product' key={index}>
-                  <ProductThumbnail
-                    className='thumbnail'
-                    src={item.product.thumbnail}
-                    size='70px'
-                  />
-
-                  <div className='product-info'>
-                    <div className='name'>{item.product.name}</div>
-                    <div className='quantity-price'>
-                      <span className='quantity'>x {item.quantity}</span>
-                      <Price fontSize='14px'>{item.product.price * item.quantity}</Price>
-                    </div>
-                  </div>
-                </Row>
-              ))}
-
-              <hr />
-
-              <OrderStatus status={order.status} />
-
-              <hr />
-
-              <Row className='total-price' justifyContent='space-between'>
-                {order.status === 'COMPLETED' && (
-                  <LinkButton textOnly to={'/leave-review/' + order.id}>
-                    Leave review
-                  </LinkButton>
-                )}
-                <span></span>
-                <Price fontSize='16px' label='Total'>
-                  {order.totalPrice}
-                </Price>
-              </Row>
-            </div>
+            <OrderItem order={order} removeOrder={removeOrder} key={index} />
           ))}
         </Div>
       </Container>
@@ -85,48 +41,6 @@ const Div = styled.div`
   grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
   grid-gap: 0.5em;
   justify-content: center;
-
-  .order-item {
-    width: 100%;
-    margin: 0.5em 0;
-    padding: 1.5em 1em;
-    position: relative;
-    border: 2px solid var(--lightgrey);
-    border-radius: 5px;
-
-    .product {
-      .thumbnail {
-        margin-right: 1.2em;
-      }
-      .name {
-        margin-bottom: 0.6em;
-      }
-      .quantity-price {
-        font-size: 14px;
-        .quantity {
-          margin-right: 1em;
-        }
-      }
-    }
-
-    hr {
-      margin: 1.2em;
-    }
-
-    .total-price {
-      margin-top: 1em;
-      button {
-        font-size: 12px;
-      }
-    }
-
-    .remove-btn {
-      position: absolute;
-      top: 10px;
-      right: 10px;
-      font-size: 11px;
-    }
-  }
 
   @media ${device.narrow} {
     padding: 0 3%;
