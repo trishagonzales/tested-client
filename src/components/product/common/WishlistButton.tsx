40bb28beaadde77a2b9bc4832dbeaeb9e2e09161
Trisha Gonzales <gonzalestrishaanne@gmail.com>
2021-03-15 03:55:43 +0800

    feat: disable wishlist on logout mode

--- src/components/product/common/WishlistButton.tsx
+++ src/components/product/common/WishlistButton.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { useToasts } from 'react-toast-notifications';
 import styled from 'styled-components';
+import { useGlobal } from '../../../hooks/common/useGlobal';
 import { useWishlist } from '../../../hooks/user/useWishlist';
 
 export interface WishlistButtonProps {
@@ -9,13 +11,22 @@ export interface WishlistButtonProps {
 
 const WishlistButton: React.FC<WishlistButtonProps> = ({ productID, className }) => {
   const { addItem, deleteItem, isAdded } = useWishlist(productID);
+  const {
+    globalState: { user },
+  } = useGlobal();
+  const { addToast } = useToasts();
 
   return (
     <Heart
       className={`${className} ${isAdded() ? 'fas' : 'far'} fa-heart`}
       onClick={() => {
-        if (isAdded()) deleteItem();
-        if (!isAdded()) addItem();
+        if (!user) {
+          addToast('Login required', { appearance: 'error' });
+        } else if (isAdded()) {
+          deleteItem();
+        } else if (!isAdded()) {
+          addItem();
+        }
       }}></Heart>
   );
 };
